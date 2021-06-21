20a86983de0e065b59e92c4a796349d00c010d44
MichaelMcFaddenjr <michaelpmcfaddenjr@gmail.com>
2021-06-21 10:50:53 -0600

    wip

--- api/server.js
+++ api/server.js
@@ -2,9 +2,18 @@ const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
 
+const { restricted } = require
+
 const server = express()
 server.use(express.json())
 server.use(helmet())
 server.use(cors())
+
+server.use((err, req, res, next) => { //eslint-disable-line
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack
+  })
+})
 
 module.exports = server
